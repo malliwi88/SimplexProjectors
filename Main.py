2c5b52c5ee89d36aa5c6aebba30dcb9f8e55815b
Stuart Gordon Reid <StuartGordonReid@gmail.com>
2015-07-02 09:03:13 +0200

    Stupid PYCHARMS

--- Main.py
+++ Main.py
@@ -6,24 +6,32 @@ from Barebones import BarebonesOptimizer
 from Portfolio import Portfolio
 import matplotlib.pylab as plt
 import numpy.random as nrand
+import matplotlib
 import pandas
 import numpy
 
 
 def plot(r, sim):
+    fig = plt.figure()
+    plt.style.use('grayscale')
     asset_prices = sim.asset_prices(n, returns=r)
+    i = 1
     for p in asset_prices:
-        plt.plot(p)
+        plt.plot(p, label="Security " + str(i))
+        i += 1
+    plt.xlabel("Time")
+    plt.ylabel("Price")
     plt.show()
 
 if __name__ == '__main__':
-    n, sigma, delta, mu, time = 15, 0.125, float(1/252), 0.08, 500
+    n, sigma, delta, mu, time = 30, 0.125, float(1/252), 0.01, 500
     asset_simulator = AssetSimulator(delta, sigma, mu, time)
     asset_returns = asset_simulator.assets_returns(n)
     corr = pandas.DataFrame(asset_returns).transpose().corr()
     portfolio = Portfolio(asset_returns, corr)
-    # plot(asset_returns, asset_simulator)
+    plot(asset_returns, asset_simulator)
 
+    """
     pso = None
     pso = BarebonesOptimizer(25, asset_returns, corr)
     pso.optimize_preserving(5001)
@@ -35,3 +43,4 @@ if __name__ == '__main__':
     pso = None
     pso = BarebonesOptimizer(25, asset_returns, corr)
     pso.optimize_lagrange(5001)
+    """
